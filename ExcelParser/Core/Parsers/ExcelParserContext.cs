c3fcb93e406d672dd33810ddf74b0941e473b25c
novikovsn87@gmail.com <novikovsn87@gmail.com>
2025-04-26 01:36:42 +0400

    Автовыбор стратегии в зависимости от кол-ва строк в файле

--- ExcelParser/Core/Parsers/ExcelParserContext.cs
+++ ExcelParser/Core/Parsers/ExcelParserContext.cs
@@ -1,10 +1,38 @@
 ﻿using ExcelParser.Core.Abstractions;
+using ExcelParser.Core.Parsers.Strategies;
+
 
 namespace ExcelParser.Core.Parsers;
 
-
-public class ExcelParserContext
+/// <summary>
+/// Контекст парсера Excel, автоматически выбирающий стратегию в зависимости от количества строк.
+/// </summary>
+public static class ExcelParserContext
 {
-    
+    public static async Task<List<TModel>> ParseAsync<TModel>(IExcelSheet sheet) where TModel : new()
+    {
+        if (sheet.StartRow == 0 || sheet.EndRow == 0)
+            return [];
+
+        int rowsCount = sheet.EndRow - sheet.StartRow;
+
+        IExcelParseStrategy strategy;
+
+        if (rowsCount < 1000)
+        {
+            strategy = new SimpleParseStrategy();
+        }
+        else if (rowsCount < 10000)
+        {
+            strategy = new ParallelParseStrategy();
+        }
+        else
+        {
+            strategy = new DataflowParseStrategy();
+        }
+
+        return await strategy.ParseAsync<TModel>(sheet);
+    }
 }
+
 
