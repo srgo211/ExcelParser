ae8ea358ce1093c59ca122d305c5c956904c82e9
novikovsn87@gmail.com <novikovsn87@gmail.com>
2025-04-26 01:00:43 +0400

    создание интерфейса для представления листа Excel

---
+++ ExcelParser/Core/Abstractions/IExcelSheet.cs
@@ -0,0 +1,23 @@
+﻿namespace ExcelParser.Core.Abstractions;
+
+/// <summary>Абстракция для представления листа Excel, независимого от конкретной библиотеки (EPPlus, ClosedXML и т.д.) </summary>
+public interface IExcelSheet
+{
+    /// <summary>Номер первой строки с данными.</summary>
+    int StartRow { get; }
+
+    /// <summary>Номер последней строки с данными.</summary>
+    int EndRow { get; }
+
+    /// <summary>Номер первого столбца с данными.</summary>
+    int StartColumn { get; }
+
+    /// <summary>Номер последнего столбца с данными.</summary>
+    int EndColumn { get; }
+
+    /// <summary>Получает текстовое значение ячейки по указанной позиции</summary>
+    /// <param name="row">Номер строки (1-based).</param>
+    /// <param name="column">Номер столбца (1-based).</param>
+    /// <returns>Текстовое значение ячейки, либо пустая строка.</returns>
+    string GetCellValue(int row, int column);
+}
