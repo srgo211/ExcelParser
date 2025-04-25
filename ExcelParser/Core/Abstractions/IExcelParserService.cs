17d904c8b4661a4c42edaa5e878cd6d1092e2846
novikovsn87@gmail.com <novikovsn87@gmail.com>
2025-04-26 01:42:20 +0400

    Реализация сервиса для DI ExcelParserService

---
+++ ExcelParser/Core/Abstractions/IExcelParserService.cs
@@ -0,0 +1,8 @@
+﻿namespace ExcelParser.Core.Abstractions;
+
+/// <summary>Сервис для универсального парсинга Excel-листов в модели.</summary>
+public interface IExcelParserService
+{
+    /// <summary>Парсит Excel-лист в список моделей</summary>
+    Task<List<TModel>> ParseAsync<TModel>(IExcelSheet sheet) where TModel : new();
+}
---
+++ ExcelParser/Core/Parsers/ExcelParserService.cs
@@ -0,0 +1,12 @@
+﻿using ExcelParser.Core.Abstractions;
+
+namespace ExcelParser.Core.Parsers;
+
+/// <summary>реализация сервиса парсинга Excel</summary>
+public class ExcelParserService : IExcelParserService
+{
+    public async Task<List<TModel>> ParseAsync<TModel>(IExcelSheet sheet) where TModel : new()
+    {
+        return await ExcelParserContext.ParseAsync<TModel>(sheet);
+    }
+}
