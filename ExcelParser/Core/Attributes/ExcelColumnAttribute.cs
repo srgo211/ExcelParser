da2c52cd56a23297efc72d9623716884dceb5b5a
novikovsn87@gmail.com <novikovsn87@gmail.com>
2025-04-26 01:03:40 +0400

    Атрибуты для мапинга

---
+++ ExcelParser/Core/Attributes/ExcelColumnAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace ExcelParser.Core.Attributes;
+
+/// <summary> Атрибут для указания, к какой колонке Excel привязано свойство модели. </summary>
+[AttributeUsage(AttributeTargets.Property)]
+public sealed class ExcelColumnAttribute : Attribute
+{
+    /// <summary>Название колонки Excel.</summary>
+    public string ColumnName { get; }
+
+    public ExcelColumnAttribute(string columnName)
+    {
+        ColumnName = columnName ?? throw new ArgumentNullException(nameof(columnName));
+    }
+}
